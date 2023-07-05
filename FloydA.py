7bd3d0ed29222f9064e520940ff7938b897ce1ef
Steve Blades <steve@elephants.co.za>
2023-07-05 14:47:46 +0200

    Inclusion of Iterative Comparison

--- FloydA.py
+++ FloydA.py
@@ -31,5 +31,27 @@ def shortestdistance(matrix, start_node, end_node, intermediate):
     return min_value
 
 
-output = floydA(graph)
-print(output)
+def floydAIterative(matrix):
+    V = len(matrix[0])
+    dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
+    for k in range(V):
+
+        # pick all vertices as source one by one
+        for i in range(V):
+
+            # Pick all vertices as destination for the
+            # above picked source
+            for j in range(V):
+
+                # If vertex k is on the shortest path from
+                # i to j, then update the value of dist[i][j]
+                dist[i][j] = min(dist[i][j],
+                                 dist[i][k] + dist[k][j]
+                                 )
+    return dist
+
+
+recursiveoutput = floydA(graph)
+print(recursiveoutput)
+interativeoutput = floydAIterative(graph)
+print(interativeoutput)
---
+++ TestfloydA.py
@@ -0,0 +1,4 @@
+import unittest
+class TestfloydA(unittest.TestCase):
+    def testfloyd(self):
+        self.assertEqual("Test Failed")
