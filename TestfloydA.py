bf5287dfdd438ebc50e9433121ac109a7b76f10a
Steve Blades <steve@elephants.co.za>
2023-07-06 13:03:58 +0200

    Add unit tests

--- TestfloydA.py
+++ TestfloydA.py
@@ -1,4 +1,48 @@
 import unittest
+import testexamples as t
+import FloydA
 class TestfloydA(unittest.TestCase):
-    def testfloyd(self):
-        self.assertEqual("Test Failed")
+    def testfloyd1(self):
+        floyaAOutput = t.input_1
+        floyaAOutput = FloydA.floydA(floyaAOutput)
+        memoOutput = t.output_1
+        self.assertEqual(floyaAOutput,memoOutput,"Test on Input_1 Failed")
+
+    def testfloyd2(self):
+        floyaAOutput = t.input_2
+        floyaAOutput = FloydA.floydA(floyaAOutput)
+        memoOutput = t.output_2
+        self.assertEqual(floyaAOutput,memoOutput,"Test on Input_2 Failed")
+
+    def testfloyd3(self):
+        floyaAOutput = t.input_3
+        floyaAOutput = FloydA.floydA(floyaAOutput)
+        memoOutput = t.output_3
+        self.assertEqual(floyaAOutput,memoOutput,"Test on Input_3 Failed")
+
+    def testfloyd4(self):
+        floyaAOutput = t.input_4
+        floyaAOutput = FloydA.floydA(floyaAOutput)
+        memoOutput = t.output_4
+        self.assertEqual(floyaAOutput,memoOutput,"Test on Input_4 Failed")
+
+    def testfloyd5(self):
+        floyaAOutput = t.input_5
+        floyaAOutput = FloydA.floydA(floyaAOutput)
+        memoOutput = t.output_5
+        self.assertEqual(floyaAOutput,memoOutput,"Test on Input_5 Failed")
+
+    def testfloyd6(self):
+        floyaAOutput = t.input_6
+        floyaAOutput = FloydA.floydA(floyaAOutput)
+        memoOutput = t.output_6
+        self.assertEqual(floyaAOutput,memoOutput,"Test on Input_6 Failed")
+
+    def testfloyd7(self):
+        floyaAOutput = t.input_7
+        floyaAOutput = FloydA.floydA(floyaAOutput)
+        memoOutput = t.output_7
+        self.assertEqual(floyaAOutput,memoOutput,"Test on Input_7 Failed")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ testexamples.py
@@ -0,0 +1,69 @@
+import sys
+NO_PATH = float('inf')
+# geeks example
+input_1 = [[0, 5, NO_PATH, 10],
+           [NO_PATH, 0, 3, NO_PATH],
+           [NO_PATH, NO_PATH, 0,   1],
+           [NO_PATH, NO_PATH, NO_PATH, 0]]
+
+output_1 = [[0, 5, 8, 9],
+            [NO_PATH, 0, 3, 4],
+            [NO_PATH, NO_PATH, 0,   1],
+            [NO_PATH, NO_PATH, NO_PATH, 0]]
+# github negative edge example
+
+input_2 = [[0, NO_PATH, 9, NO_PATH],
+           [NO_PATH, 0, NO_PATH, NO_PATH],
+           [NO_PATH, -5, 0, 8],
+           [-3, 2, NO_PATH, 0]]
+
+output_2 = [[0, 4, 9, 17],
+            [NO_PATH, 0, NO_PATH, NO_PATH],
+            [5, -5, 0, 8],
+            [-3, 1, 6, 0]]
+
+# 2 vertex graph example
+
+input_3 = [[0, 2],
+           [NO_PATH, 0]]
+
+output_3 = [[0, 2],
+            [NO_PATH, 0]]
+
+# 2 vertex graph (no path) example
+
+input_4 = [[0, NO_PATH],
+           [NO_PATH, 0]]
+
+output_4 = [[0, NO_PATH],
+            [NO_PATH, 0]]
+
+# 1 vertex graph example
+
+input_5 = [[0]]
+
+output_5 = [[0]]
+
+# youtube example
+
+input_6 = [[0, 3, NO_PATH, 7],
+           [8, 0, 2, NO_PATH],
+           [5, NO_PATH, 0, 1],
+           [2, NO_PATH, NO_PATH, 0]]
+
+output_6 = [[0, 3, 5, 6],
+            [5, 0, 2, 3],
+            [3, 6, 0, 1],
+            [2, 5, 7, 0]]
+
+# pre-brief example
+
+input_7 = [[0, 7, NO_PATH, 8],
+           [NO_PATH, 0, 5, NO_PATH],
+           [NO_PATH, NO_PATH, 0, 2],
+           [NO_PATH, NO_PATH, NO_PATH, 0]]
+
+output_7 = [[0, 7, 12, 8],
+            [NO_PATH, 0, 5, 7],
+            [NO_PATH, NO_PATH, 0, 2],
+            [NO_PATH, NO_PATH, NO_PATH, 0]]
